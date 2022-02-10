6e76db89ee1d25fedca7fa0fe4d4c04590ed0145
Mizanur888 <rahmanm2@wit.edu>
2022-02-10 10:42:01 -0500

    insertElementInSortedArray

--- AlgoTest/Program.cs
+++ AlgoTest/Program.cs
@@ -12,7 +12,7 @@ namespace AlgoTest
 
         public static void countStringCombination()
         {
-            string combination = "aabbcdddeeettb";
+            string combination = "Inflation has been driven higher by soaring demand and lack of supply caused by Covid pandemic’s global impact on trade";
             int count = 0;
             string temp = "";
             char[] arr = new char[combination.Length];
@@ -23,24 +23,31 @@ namespace AlgoTest
             for (int i = 0; i < arr.Length; i++)
             {
                 char current = arr[i];
-                count = 0;
-                if (!holdChar.Contains(current))
-                {
-                    for (int k = i; k < arr.Length; k++)
+
+                Console.WriteLine(current);
+
+                    count = 0;
+                    if (!holdChar.Contains(current))
                     {
-                        if (current == arr[k])
+
+                        for (int k = i; k < arr.Length; k++)
                         {
-                            count++;
-                            holdChar[i] = arr[i];
+                            if (current == arr[k])
+                            {
+                                count++;
+                                holdChar[i] = arr[i];
+                            }
                         }
                     }
-                }
-                if (count != 0)
-                    temp += arr[i] + count.ToString();
+                    if (count != 0)
+                        temp +=arr[i]!=' '? arr[i] + count.ToString(): 'S' + count.ToString();
+                
+
             }
 
 
             Console.WriteLine(temp);
+            
         }
     }
 }
